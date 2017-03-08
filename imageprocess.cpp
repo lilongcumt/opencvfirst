fb32ecd296c298babf9468bfc10c6a9e301ae062
lilongcumt <lilongcumt@163.com>
2017-03-09 00:07:59 +0800

    加入图像处理函数

--- imageprocess.cpp
+++ imageprocess.cpp
@@ -9,7 +9,7 @@ bool Imageprocess::separateImage(const Mat &src, Mat *dst, Range row, Range col)
 {
     if(!src.empty()){
         *dst=src(row,col).clone();
-        return ture;
+        return true;
     }
     return false;
 }
@@ -26,7 +26,38 @@ bool Imageprocess::adjustBrightness(const Mat &src, Mat *dst, double alpha, doub
     dst->create(src.size(),src.type());
     for(int y=0;y<height;++y){
         for(int x=0;x<width;++x){
-
+            dst->at<uchar>(y,x)=saturate_cast<uchar>(alpha*src.at<uchar>(y,x)+beta);
         }
     }
+    return true;
+}
+
+bool Imageprocess::findEdge(const Mat &src, Mat *dst, int threshold)
+{
+    if(src.channels()!=1){
+        std::cout << "this method only take care of gray value image" << std::endl;
+        return false;
+    }
+
+    dst->create(src.size(),src.type());
+    Mat temp(src.size(),src.type());
+    temp=src.clone();
+
+    Canny(temp,*dst,threshold,threshold);
+
+}
+
+bool Imageprocess::morphologyProcess(const Mat &src, Mat *dst, int ksize)
+{
+    if(src.channels()!=1){
+        std::cout << "this method only take care of gray value image" << std::endl;
+        return false;
+    }
+    dst->create(src.size(),src.type());
+
+    Mat element = getStructuringElement(0,Size(ksize,ksize));
+
+    morphologyEx(src,(*dst),MORPH_CLOSE,element);
+
+    return true;
 }
--- mainwindow.cpp
+++ mainwindow.cpp
@@ -20,7 +20,7 @@ bool MainWindow::loadImage(Mat *dstMat, QImage *dstImage)
     QString filename=QFileDialog::getOpenFileName(
                 this,
                 tr("打开图片"),
-                "./../opencfirst/picture",
+                "F:/qtcode/picture",
                 tr("图片格式(*.png *.jpg *.bmp)")
                 );
     if(filename.isEmpty()){
@@ -34,6 +34,30 @@ bool MainWindow::loadImage(Mat *dstMat, QImage *dstImage)
     //解决Qlabel显示OpenCV中Mat图像数据，对齐
     QImage image4show(dstMat->data,dstMat->cols,dstMat->rows,dstMat->step,QImage::Format_Grayscale8);
     *dstImage=image4show;
+
+
+    namedWindow("first");
+    namedWindow("second");
+    namedWindow("third");
+    namedWindow("fourth");
+
+//    Mat tempImage,tempImage1,tempImage2,tempImage3,tempImage4;
+
+//    medianBlur(m_Image,tempImage,1);
+
+//    Imageprocess::adjustBrightness(m_Image,&tempImage1,2,5);
+//    imshow("first",tempImage1);
+
+//    Imageprocess::findEdge(tempImage1,&tempImage2,50);
+//    imshow("second",tempImage2);
+
+//    Imageprocess::morphologyProcess(tempImage2,&tempImage3,2);
+//    imshow("third",tempImage3);
+
+//    Imageprocess::findEdge(tempImage3,&tempImage4,3);
+//    imshow("fourth",tempImage4);
+
+
     return true;
 
 }
@@ -49,3 +73,4 @@ void MainWindow::on_btn_loadImage_clicked()
     ui->lab_Image->setPixmap(QPixmap::fromImage(imageSccled));
     qDebug()<<m_Image.rows<<m_Image.cols;
 }
+
